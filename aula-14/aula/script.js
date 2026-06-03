27b711e067bd8b1e898c163fa1c3a9c040d1e708
Eduardo-FM <eduardo.freitas.machado@gmail.com>
2026-06-03 20:44:07 -0300

    aula

---
+++ aula-14/aula/script.js
@@ -0,0 +1,10 @@
+//document.writeln('Olá mundo');
+
+//window.alert('Olá, código JavaScript!')
+
+//document.getElementById('caixa').innerHTML = 'Olá, Mundo!';
+
+function enviarFormulario() {
+    var IU = document.getElementById('nome').value;
+    window.alert('Foi digitado: ' + IU)
+}
